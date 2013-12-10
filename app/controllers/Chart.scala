f8404b8e2b46dbbb9176da17841042dd902fee60
Gadiel Ortez <gadiel.ortez@unitec.edu>
2013-12-09 19:36:27 -0600

    olakehace funciona o kehace

---
+++ app/controllers/Chart.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import play.api.Play.current
+import play.api._
+import play.api.mvc._
+import play.api.db._
+
+object Chart extends Controller {
+
+  def index = Action {
+  	var outString="Numer is"
+  	val conn = DB.getConnection("default", true)
+  	try {
+  		val stmt= conn.createStatement()
+  		val rs = stmt.executeQuery("SELECT * FROM Users")
+  		while(rs.next()) {
+  			outString += rs.getString("email")
+  		}
+  	} finally {
+  		conn.close()
+  	}	
+  		
+    Ok(views.html.index(outString))
+  }
+  
+  
+
+}
